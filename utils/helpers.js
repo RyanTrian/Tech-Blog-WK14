b3be93d935d30a604df6176b914cf2321563ed7e
Minh Truong <mptruong49@gmail.com>
2022-12-04 13:53:19 -0700

    Utils, helper functions

---
+++ utils/helpers.js
@@ -0,0 +1,6 @@
+// Format Date to MM/DD/YYYY
+module.exports = {
+    format_date: date => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+      },
+}
